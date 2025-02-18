df4089fe58236df0e285cc99967a98b1618345e3
dev-ak12 <95645827+dev-ak12@users.noreply.github.com>
2025-02-18 22:30:27 +0530

    Add Django API

---
+++ backendDjango/chatServer/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Conversation(models.Model):
+    user_message = models.TextField()
+    bot_response = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
---
+++ backendDjango/chatServer/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render
+
+# Create your views here.
+from openai import OpenAI
+
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Conversation
+from .serializers import ConversationSerializer
+from django.conf import settings
+
+# Set Key as env_var before running code
+client = OpenAI()
+
+@api_view(['POST'])
+def chat(request):
+    user_message = request.data.get('message', '')
+
+    conversation_history = [
+    {"role": "system", "content": f"You are a highly sarcastic and witty AI. Respond to the following statement with a humorous and sarcastic remark"}
+    ]
+
+    userQuery = { "role": "user", "content": user_message }
+    conversation_history.append(userQuery)
+    response = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages= conversation_history,
+    temperature=0.7,
+    )
+    conversation_history.append({"role": "assistant", "content": response.choices[0].message.content})
+
+    bot_reply = response.choices[0].message.content
+
+    # Save conversation to DB
+    # conversation = Conversation.objects.create(user_message=user_message, bot_response=bot_reply)
+
+    return Response({"reply": bot_reply})
